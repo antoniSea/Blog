4fe6215b823b27356a8e9359938491f1f1660ce9
Antoni Seba <antoniwoj@o2.pl>
2023-06-22 18:23:55 +0200

    Changes

--- Client/dist/src/Routes/users.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import userControllers from '../Controllers/UserController.js';
-import auth from '../Middleware/AuthMiddleware.js';
-const router = express.Router();
-router.get('/all-users', userControllers.getAll);
-router.get('/users/:id', userControllers.getById);
-router.put('/users/:id', userControllers.update);
-router.post('/toggle-dark-mode', auth.loggedIn, userControllers.toggleDarkMode);
-export default router;
