729b8cb51961c48c0ef64ba8ea864898a3ce8748
Antoni Seba <antoniwoj@o2.pl>
2023-06-22 18:21:51 +0200

    Delete Client/dist directory

--- Client/dist/src/Routes/auth.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import userControllers from '../Controllers/UserController.js';
-import auth from '../Middleware/AuthMiddleware.js';
-const router = express.Router();
-router.post('/require-token', userControllers.requireToken);
-router.post('/create-user', auth.loggedIn, userControllers.create);
-router.get('/user', auth.loggedIn, userControllers.getUser);
-export default router;
